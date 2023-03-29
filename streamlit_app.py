8ba73aa871d64ebe5da05016f2fd0dc5c53bf877
reneeData <129334369+reneeData@users.noreply.github.com>
2023-03-29 17:22:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,7 @@ streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 
 streamlit.write('Omega 3 & Blueberry Oatmeal')
+
+streamlit.write('Kale, Spinach & Rocket Smoothie')
+
+stramlit.write('Hard-Boiled Free_Range Egg')
