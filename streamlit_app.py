234ba0b24e4ab516272b643944c061c4f383e406
reneeData <129334369+reneeData@users.noreply.github.com>
2023-03-30 08:00:29 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('🍜Breakfast Favorites')
+streamlit.header('Breakfast Favorites')
 
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 
